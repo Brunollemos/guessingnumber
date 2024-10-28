b795a2558cbfbdb524a3e99a65ceab5a212e87fb
Bruno Lemos <brunollemos@live.com>
2024-10-28 17:57:41 -0300

    criando função com alert

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    let numeroSecreto = 7;
+    let chute = parseInt(prompt('Digite um número entre 1 e 10'));
+
+    if (chute === numeroSecreto) {
+        alert('Parabéns, você acertou!');
+    } else {
+        alert('Que pena, tente novamente!');
+        verificarChute();
+    }
+}
+
+verificarChute();
