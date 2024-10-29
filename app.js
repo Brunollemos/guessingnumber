8cc18b37b5c4451ed579d9826663d4efa3c7927a
Bruno Lemos <brunollemos@live.com>
2024-10-29 20:56:13 -0300

    função com retorno: gerarNumeroAleatorio

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Jogo do número secreto';
+let numeroSecreto = gerarNumeroAleatorio();
 
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;    
 }
 
+function verificarChute(chute) {
+    console.log(numeroSecreto);    
+}
+
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-
-function verificarChute() {
-    /*let numeroSecreto = 7;
-    let chute = parseInt(prompt('Digite um número entre 1 e 10'));
-
-    if (chute === numeroSecreto) {
-        alert('Parabéns, você acertou!');
-    } else {
-        alert('Que pena, tente novamente!');
-        verificarChute();
-    }*/
-}
 
 verificarChute();
