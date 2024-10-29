44f56caf802220728428c51bbbeb1f2d34d9fc0d
Bruno Lemos <brunollemos@live.com>
2024-10-29 19:59:51 -0300

    função com parâmetro

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;    
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    let numeroSecreto = 7;
+    /*let numeroSecreto = 7;
     let chute = parseInt(prompt('Digite um número entre 1 e 10'));
 
     if (chute === numeroSecreto) {
@@ -13,7 +21,7 @@ function verificarChute() {
     } else {
         alert('Que pena, tente novamente!');
         verificarChute();
-    }
+    }*/
 }
 
 verificarChute();
