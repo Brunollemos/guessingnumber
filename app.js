ca0b9086c7400aa73956238c7b772a524040542f
Bruno Lemos <brunollemos@live.com>
2024-10-30 21:10:44 -0300

    reiniciando jogo

--- app.js
+++ app.js
@@ -5,9 +5,27 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
 }
 
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();    
+    limparCampo();
+    tentativas = 0;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    exibirMensagemInicial();
+}
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;    
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 function verificarChute() {
@@ -17,6 +35,8 @@ function verificarChute() {
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');            
@@ -24,10 +44,8 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior');            
         }
         tentativas++;
+        limparCampo();
     }
 }
-
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
-
+exibirMensagemInicial();
 verificarChute();
